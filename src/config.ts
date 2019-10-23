1464993fcfd2be9b987d88e3ac512b425fd25922
Yuriy Boev <yuriy.boev@blue-leaf.co.uk>
2019-10-24 00:26:04 +0300

    Converts tabs to spaces

--- src/config.ts
+++ src/config.ts
@@ -3,19 +3,19 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 export const TRANSITION_IDS = {
-	IN_PROGRESS: process.env.TRANSITION_IN_PROGRESS,
-	AWAITING_REVIEW: process.env.TRANSITION_AWAITING_REVIEW,
-	APPROVE_REVIEW: process.env.TRANSITION_APPROVE_REVIEW,
-	REJECT_REVIEW: process.env.TRANSITION_REJECT_REVIEW
+    IN_PROGRESS: process.env.TRANSITION_IN_PROGRESS,
+    AWAITING_REVIEW: process.env.TRANSITION_AWAITING_REVIEW,
+    APPROVE_REVIEW: process.env.TRANSITION_APPROVE_REVIEW,
+    REJECT_REVIEW: process.env.TRANSITION_REJECT_REVIEW
 }
 export const JIRA = {
-	PROTOCOL: process.env.JIRA_PROTOCOL,
-	HOST: process.env.JIRA_HOST,
-	BASE: process.env.JIRA_BASE,
-	USERNAME: process.env.JIRA_USERNAME,
-	PASSWORD: process.env.JIRA_PASSWORD,
-	API_VERSION: process.env.JIRA_API_VERSION,
-	STRICT_SSL: process.env.JIRA_STRICT_SSL
+    PROTOCOL: process.env.JIRA_PROTOCOL,
+    HOST: process.env.JIRA_HOST,
+    BASE: process.env.JIRA_BASE,
+    USERNAME: process.env.JIRA_USERNAME,
+    PASSWORD: process.env.JIRA_PASSWORD,
+    API_VERSION: process.env.JIRA_API_VERSION,
+    STRICT_SSL: process.env.JIRA_STRICT_SSL
 }
 export const NODE_ENV = process.env.NODE_ENV;
 export const PORT = process.env.PORT;
